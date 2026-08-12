0135dd9ad5b8a46d391cb5b45f80aba6c5ef410e
puneet datla <datlapuneet@gmail.com>
2026-08-12 12:59:33 -0400

    add tenant data structure

--- app.py
+++ app.py
@@ -3,3 +3,11 @@ print("Welcome to the Multi-Tenant SaaS Platform")
 tenant_name = input("Enter your company name: ")
 
 print("Welcome,", tenant_name)
+
+
+tenant = {
+    "name": tenant_name,
+    "plan": "Free",
+    "users": 1
+}
+print(tenant)

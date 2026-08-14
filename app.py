91c5818efff910bd6f7db1eaa60f003bf97e2ca5
puneet datla <datlapuneet@gmail.com>
2026-08-13 23:16:53 -0400

    Add interactive tenant search

--- app.py
+++ app.py
@@ -17,3 +17,34 @@ tenants.append(tenant2)
 
 for tenant in tenants:
     print(tenant["name"])
+
+
+def find_tenant(name):
+    for tenant in tenants:
+        if tenant["name"] == name:
+            return tenant
+
+    return None
+
+
+result = find_tenant("Netflix")
+
+print(result)
+
+
+search_name = input("enter the tenant name: ")
+result = find_tenant(search_name)
+print(result)
+
+
+search_name = input("Enter tenant name: ")
+
+result = find_tenant(search_name)
+
+if result:
+    print("Tenant found!")
+    print("Name:", result["name"])
+    print("Plan:", result["plan"])
+    print("Users:", result["users"])
+else:
+    print("Tenant not found.")
